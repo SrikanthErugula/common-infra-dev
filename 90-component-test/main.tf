086f87fb1959a9f17f0f61764376ccfebf0fc67d
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-02-06 11:41:13 +0530

    terraform_start

--- 90-component-test/main.tf
+++ 90-component-test/main.tf
@@ -1,14 +1,16 @@
-module "component" {           #sess-46
-    source = "../tform-robo-component"
-    component = var.component
-    rule_priority = var.rule_priority 
+# module "component" {           #sess-46
+#     source = "../../tform-robo-component"
+#     component = var.component
+#     rule_priority = var.rule_priority 
+# }
+
+
+
+module "components" {
+    for_each = var.components 
+    source = "git::https://github.com/SrikanthErugula/tform-robo-component.git?ref=main"
+    component = each.key
+    rule_priority = each.value.rule_priority
 }
 
-
-
-# module "components" {
-#     for_each = var.components
-#     source = "git::https://github.com/daws-86s/terraform-roboshop-component.git?ref=main"
-#     component = each.key
-#     rule_priority = each.value.rule_priority
-# }
+# so ikkada source change chesam give terraform init -reconfigure 
--- 90-component-test/variable.tf
+++ 90-component-test/variable.tf
@@ -1,4 +1,6 @@
-variable "component" {          # SESS-46
+ # SESS-46
+
+variable "component" {
     default = "catalogue"
 }
 
@@ -6,25 +8,28 @@ variable "rule_priority" {  # tform-rob module lo declared
     default = 10
 }
 
-# variable "components" {
-#     default = {
-#         catalogue = {
-#             rule_priority = 10
-#         }
-#         user = {
-#             rule_priority = 20
-#         }
-#         cart = {
-#             rule_priority = 30
-#         }
-#         shipping = {
-#             rule_priority = 40
-#         }
-#         payment = {
-#             rule_priority = 50
-#         }
-#         frontend = {
-#             rule_priority = 10
-#         }
-#     }
-# }
+variable "components" {
+    default = {
+        catalogue = {
+            rule_priority = 10
+        }
+        user = {
+            rule_priority = 20
+        }
+        cart = {
+            rule_priority = 30
+        }
+        shipping = {
+            rule_priority = 40
+        }
+        payment = {
+            rule_priority = 50
+        }
+        frontend = {
+            rule_priority = 10
+        }
+    }
+}
+
+# here rule priority same ga vundakudadhu which evevr connect backend_alb
+# so frontend rule priority 10 ichhina no issues bcz dhaniki connnect ayye ALB is different 
