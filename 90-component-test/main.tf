4f858d1e252e46703292c1dc7282b0b7b4c9b47d
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-07-14 22:05:53 +0530

    modified_code

--- 90-component-test/main.tf
+++ 90-component-test/main.tf
@@ -9,8 +9,8 @@
 module "components" {
     for_each = var.components 
     source = "git::https://github.com/SrikanthErugula/tform-robo-component.git?ref=main"
-    component = each.key
-    rule_priority = each.value.rule_priority
+    component = each.key #( each.key lo values are like cata, user, cart...)
+    rule_priority = each.value.rule_priority  # ( each.value lo values are cata.10, user.20, cart.30..... )
 }
 
 # so ikkada source change chesam give terraform init -reconfigure 
